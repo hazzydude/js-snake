05996507f68090a570094b6f754e1ece72041c71
hazzydude <haydenlamb97@gmail.com>
2020-05-22 13:46:01 +0100

    update

---
+++ snake-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MyCanvas from "./components/myCanvas"
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <MyCanvas />
+     
+    </div>
+  );
+}
+
+export default App;
